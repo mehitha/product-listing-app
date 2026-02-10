9644effc6fae7d1875972f50c8815b93a80bdca5
mehitha <angelinmehitha@gmail.com>
2026-02-10 16:28:22 +0530

    Fix API URL and backend routes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
+
+// const API_URL = "http://localhost:5000";
+// const API_URL = "https://product-listing-app-1-utjy.onrender.com";
+const API_URL = "https://product-listing-tixc.onrender.com/api";
+
+axios.get(`${API_URL}/products`);
+
+
 
 export const getProducts = () => {
   return axios.get(`${API_URL}/products`);
@@ -11,16 +18,32 @@ export const addProduct = (product) => {
 };
 
 
-// src/api.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import axios from "axios";
 
-// // Use your deployed backend URL here
-// const API_URL = "https://your-live-backend.onrender.com"; // <— live backend
+// // Use Render URL for production
+// const API_URL = "https://product-listing-tixc.onrender.com";
 
+// // Get all products
 // export const getProducts = () => {
-//   return axios.get(`${API_URL}/products`);
+//   return axios.get(`${API_URL}/api/products`);
 // };
 
+// // Add new product
 // export const addProduct = (product) => {
-//   return axios.post(`${API_URL}/products`, product);
+//   return axios.post(`${API_URL}/api/products`, product);
 // };
